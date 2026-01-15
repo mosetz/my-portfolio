4e9e737ea65c16f714163d1d23257ae1b968396f
Sarit Samkumpim <saritsamkumpim@gmail.com>
2026-01-15 20:55:34 +0700

    setup express backend tested node mailer on localhost

---
+++ backend/server.js
@@ -0,0 +1,78 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import nodemailer from 'nodemailer';
+
+dotenv.config();
+
+const app = express();
+
+/**
+ * Allow frontend to call this backend
+ */
+app.use(cors());
+
+/**
+ * Prase JSON bodies (so req.body work)
+ */
+app.use(express.json());
+
+// Health check route (quick test)
+app.get("/", (req, res) => {
+    res.send("Contact API is running");
+});
+
+app.post("/api/contact", async (req, res) => {
+
+    try {
+        const {name, email, message} = req.body;
+
+        //step 1 basic validation 
+        if(!name || !email || !message){
+            return res.status(400).json({ok: false, error: "Missing field" });
+        }
+
+        //simple email validate check
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ok: false, error: "Invalid email."});
+        }
+
+        //step2 Create transporter (Gmail)
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_APP_PASSWORD,
+            },
+        });
+
+        // step3 Email to me
+        const mailToYou = {
+            from: process.env.EMAIL_USER,
+            to: process.env.EMAIL_TO,
+            subject: `New contact form message from ${name}`,
+            replyTo: email,
+            text: `Name: ${name}\nEmail: ${email}\n\nMessage: ${message}`,
+        };
+
+        //step 4 Email to user
+        const mailToUser = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Thanks for contacting me!",
+            text: `Hi ${name}, \n\nThanks for reaching out - I received your message and will get back to you soon. \n\nYour message:\n"${message}"\n\nBest regards, \nSarit Samkumpim`,
+        };
+
+        await transporter.sendMail(mailToYou);
+        await transporter.sendMail(mailToUser);
+
+        return res.json({ok: true, message: "Email sent successfully"});
+    }catch (err) {
+        console.error(err);
+        return res.status(500).json({ok: false, message:"Server error."});
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ docs/submit.js
@@ -0,0 +1,31 @@
+const submitForm = document.querySelector("#connect_form");
+
+submitForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const message = document.querySelector("#message").value;
+
+    try{
+        const res = await fetch("http://localhost:5000/api/contact", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({name, email, message}),
+        });
+
+        const data = await res.json();
+
+        if (!data.ok){
+            alert(data.error || "Something went wrong");
+            return;
+        }
+
+        alert("Message sent! please check you email.");
+        submitForm.reset();
+    } catch (err) {
+        alert("Could not send message. Server maybe offline.");
+    }
+
+
+});
